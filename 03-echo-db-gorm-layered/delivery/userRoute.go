238e42f69b6b48ef1c7372e4ce85d98ff6aa4ac4
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2022-01-07 11:10:21 +0700

    add middlewares

--- 03-echo-db-gorm-layered/delivery/userRoute.go
+++ 03-echo-db-gorm-layered/delivery/userRoute.go
@@ -1,13 +1,44 @@
 package delivery
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/gorm"
 )
 
+func LogElapsedTime(handler echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		startTime := time.Now()
+		err := handler(c)
+		elapsed := time.Since(startTime)
+		fmt.Println(elapsed)
+		return err
+	}
+}
+
 func InitUserRoute(e *echo.Echo, DB *gorm.DB) {
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(
+		middleware.Logger(),
+		middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+			// echo dono:password | base64
+			if username == "dono" && password == "password" {
+				return true, nil
+			}
+			return false, nil
+		}),
+	)
+
+	// e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	// 	Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
+	// }))
 	e.GET("/users", CreateGetUsersController(DB))
 	e.POST("/users", CreateAddUserController(DB))
+	// e.GET("/users", CreateGetUsersController(DB), middleware.Logger())
+	// e.POST("/users", CreateAddUserController(DB), LogElapsedTime)
 	// e.GET("/users/:id", GetUserController)
 	// e.PUT("/users/:id", UpdateUserController)
 	// e.DELETE("/users/:id", DeleteUserController)
--- 03-echo-db-gorm-layered/go.mod
+++ 03-echo-db-gorm-layered/go.mod
@@ -3,7 +3,7 @@ module sirclo/restapi/db/gorm
 go 1.16
 
 require (
-	github.com/labstack/echo/v4 v4.6.1 // indirect
-	gorm.io/driver/mysql v1.2.2 // indirect
-	gorm.io/gorm v1.22.4 // indirect
+	github.com/labstack/echo/v4 v4.6.1
+	gorm.io/driver/mysql v1.2.2
+	gorm.io/gorm v1.22.4
 )
